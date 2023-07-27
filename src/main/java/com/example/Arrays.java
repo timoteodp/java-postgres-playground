919ce3c67068cf522a62b78ab3a009bb0738eb6a
Timóteo Dima Pereira <timoteodp10@outlook.com>
2023-07-27 02:09:24 +0000

    Arrays

---
+++ src/main/java/com/example/Arrays.java
@@ -0,0 +1,21 @@
+package com.example;
+
+
+public class Arrays {
+
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println(x);
+
+        x = 20;
+        System.out.println(x);
+
+        double vetor[] = {10, 20, 30,40};
+        //System.out.println(Arrays.toString(vetor));
+        System.out.println("Vetor na Posição 0 : "+ vetor[0]);
+        System.out.println("Vetor na Posição 2 : "+ vetor[1]);
+        System.out.println("Vetor na Posição 3 : "+ vetor[2]);
+        System.out.println("Vetor na Posição 4 : "+ vetor[3]);
+    }
+    
+}
