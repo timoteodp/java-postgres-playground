3ebc0d14168d5e6c95bcb8dd73aa6a975d91a03b
Timóteo Dima Pereira <timoteodp10@outlook.com>
2023-07-31 01:59:15 +0000

    Arrays list

---
+++ src/main/java/com/example/AppListas.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class AppListas {
+    Cliente[] cliente = new Cliente[5];
+    List<Cliente> listaClientes = new ArrayList<>();
+    List<Cliente> listaClientes1 = new LinkedList<>();
+
+}
